e04874305a3be5a0897502cc4cc1c80dd929809d
Joe Fearnley <joe.fearnley@gmail.com>
2026-03-09 22:35:54 -0400

    Adding initial Typescript stuff

---
+++ typescript/index.ts
@@ -0,0 +1,15 @@
+import { Input, createCliRenderer, InputRenderableEvents } from "@opentui/core"
+
+const renderer = await createCliRenderer()
+
+const input = Input({
+  placeholder: "Enter your name...",
+  width: 25,
+})
+
+input.focus()
+renderer.root.add(input)
+
+input.on(InputRenderableEvents.CHANGE, (value: string) => {
+  console.log("Value committed:", value)
+})
